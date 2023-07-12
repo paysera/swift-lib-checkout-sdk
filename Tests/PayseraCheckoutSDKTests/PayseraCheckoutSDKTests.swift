4ec99b46ea4771c04112ed6265b489a203b6b16c
Andrii Chupryna <andrii.chupryna@paysera.net>
2023-07-12 18:31:28 +0300

    add integration tests

--- Tests/PayseraCheckoutSDKTests/PayseraCheckoutSDKTests.swift
+++ Tests/PayseraCheckoutSDKTests/PayseraCheckoutSDKTests.swift
@@ -1,10 +1,39 @@
 import XCTest
+import JWTDecode
+import PayseraCommonSDK
 @testable import PayseraCheckoutSDK
 
 final class PayseraCheckoutSDKTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    private lazy var checkoutAPIClient: CheckoutAPIClient = {
+        let token = "insert_me"
+        let credentials = PSApiJWTCredentials()
+        credentials.token = try! decode(jwt: token)
+        return CheckoutAPIClientFactory.createCheckoutAPIClient(credentials: credentials)
+    }()
+    
+    func testGetPaymentTypes() throws {
+        executeNonNil(
+            checkoutAPIClient.getPaymentTypes(
+                country: "Lithuania",
+                currency: "EUR"
+            ),
+            description: "Should return payment types"
+        )
+    }
+    
+    func testGetPaymentURL() throws {
+        executeNonNil(
+            checkoutAPIClient.getPaymentURL(
+                request: CheckoutURLRequest(
+                    paymentTypeId: "",
+                    amount: "100.0",
+                    currency: "EUR",
+                    account: "",
+                    email: "test@pauysera.com",
+                    lang: "en"
+                )
+            ),
+            description: "Should return payment url"
+        )
     }
 }
