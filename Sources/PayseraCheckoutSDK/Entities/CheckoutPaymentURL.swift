5b2f5e31ac412e5ca0c543edd521185ea723b226
chuprynaandrii <139122336+chuprynaandrii@users.noreply.github.com>
2023-08-02 18:16:32 +0300

    APP-5810-2: [iOS] Create SDK for Checkout (#4)
    * APP-5810-2: [iOS] Create SDK for Checkout
    
    * make `paymentURL` optional

--- Sources/PayseraCheckoutSDK/Entities/CheckoutPaymentURL.swift
+++ Sources/PayseraCheckoutSDK/Entities/CheckoutPaymentURL.swift
@@ -2,11 +2,12 @@ import Foundation
 import ObjectMapper
 
 public final class CheckoutPaymentURL: Mappable {
-    public var paymentURL: URL!
+    public var paymentURL: URL?
     
     public init?(map: ObjectMapper.Map) {}
     
     public func mapping(map: ObjectMapper.Map) {
-        paymentURL <- map["payment_url"]
+        let paymentURI: String? = try? map.value("payment_url", default: "")
+        paymentURL = paymentURI.map(URL.init(string:)).flatMap { $0 }
     }
 }
